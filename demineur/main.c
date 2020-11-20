057619796e40fddcee9e0af54ad41a8dd01885b7
marine schwartz <m.schwartz@ludus-academie.com>
2020-11-20 17:08:53 +0100

    modification du plan (main.c)

--- demineur/main.c
+++ demineur/main.c
@@ -3,15 +3,21 @@
 /*
 PLAN:
     pour demineur debutant
-1/ génération tableau 9x9 rempli de -1
-2/ demande au joueur de choisir une case --> donner valeur 0
-3/ remplissage aléatoire du tableau avec mines(1)
+1/ génération int tableauMines 9x9 rempli de -1
+   génération char tableauAffiche 9x9 rempli de ' '
+2/ demande au joueur de choisir une case
+3/ remplissage aléatoire du tableauMines avec mines(9)
     --> compteur de mines : tant que mines>0 alors on recommence
     --> on peut mettre une mine SSI case == -1
-4/ mise-à-jour case 0 --> indiquer ombien il y a de mines autour
-5/ demande au joueur de choisir la prochaine case ou il veut tester une mine
-6/ si case vide : indiquer combien de mines autour
-   si mine : perdu !
+4/ remplissage final du tableauMines
+    --> pour chaque cellule !=9, valeur=nb de mines autour
+    --> on obtient un tableau rempli de chiffres
+5/ mise-à-jour case tableauAffiche
+    --> tableauAffiche[case] = tableauMines[case]
+    --> écrire combien il y a de mines autour de la case choisie
+6/ demande au joueur de choisir la prochaine case ou il veut tester une mine
+    --> tableauAffiche[case] = tableauMines[case] avec la case choisie
+    --> si case=9 : perdu !
 
 BUT : trouver toutes les cases sans mines (transformer toutes les cases -1 en 0)
 
